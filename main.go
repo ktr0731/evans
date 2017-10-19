b9cd880b6d842ff02fd2ec895c1e6bb1d7db230a
lycoris0731 <aizu.s1230022@gmail.com>
2017-10-19 22:24:00 +0900

    Create cli package

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"os"
+
+	"github.com/ktr0731/evans/cli"
 )
 
 func main() {
-	os.Exit(NewCLI("evans", "0.1.0").Run(os.Args[1:]))
+	os.Exit(cli.NewCLI("evans", "0.1.0").Run(os.Args[1:]))
 }
