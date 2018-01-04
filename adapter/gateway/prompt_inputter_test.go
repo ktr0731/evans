c458da2a519567201be69f0a53a233c63505864d
ktr0731 <aizu.s1230022@gmail.com>
2018-01-04 12:07:47 +0900

    add resolveMessageDependency test

--- adapter/gateway/prompt_inputter_test.go
+++ adapter/gateway/prompt_inputter_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/jhump/protoreflect/dynamic"
-	"github.com/k0kubun/pp"
 	"github.com/ktr0731/evans/entity"
 	"github.com/ktr0731/evans/tests/helper"
 	"github.com/stretchr/testify/assert"
@@ -18,21 +17,21 @@ func (p *mockPrompt) Input() string {
 	return "foo"
 }
 
+func setup(t *testing.T, fpath, pkgName, svcName string) *entity.Env {
+	set := helper.ReadProto(t, []string{fpath})
+
+	env := helper.NewEnv(t, set, helper.TestConfig().Env)
+
+	err := env.UsePackage(pkgName)
+	require.NoError(t, err)
+
+	err = env.UseService(svcName)
+	require.NoError(t, err)
+
+	return env
+}
+
 func TestPromptInputter_Input(t *testing.T) {
-	setup := func(t *testing.T, fpath, pkgName, svcName string) *entity.Env {
-		set := helper.ReadProto(t, []string{fpath})
-
-		env := helper.NewEnv(t, set, helper.TestConfig().Env)
-
-		err := env.UsePackage(pkgName)
-		require.NoError(t, err)
-
-		err = env.UseService(svcName)
-		require.NoError(t, err)
-
-		return env
-	}
-
 	t.Run("normal/simple", func(t *testing.T) {
 		env := setup(t, filepath.Join("helloworld", "helloworld.proto"), "helloworld", "Greeter")
 
@@ -66,16 +65,16 @@ func TestPromptInputter_Input(t *testing.T) {
 	//
 	// 	assert.Equal(t, `name:"foo" message:"foo"`, msg.String())
 	// })
+}
 
-	t.Run("foo", func(t *testing.T) {
-		env := setup(t, filepath.Join("nested_message", "library.proto"), "library", "Library")
+func Test_resolveMessageDependency(t *testing.T) {
+	env := setup(t, filepath.Join("nested_message", "library.proto"), "library", "Library")
 
-		rpc, err := env.RPC("BorrowBook")
-		require.NoError(t, err)
+	msg, err := env.Message("Book")
+	require.NoError(t, err)
 
-		for _, f := range rpc.RequestType.GetFields() {
-			pp.Println(f.GetName())
-			pp.Println(f.GetMessageType().GetFullyQualifiedName())
-		}
-	})
+	dep := messageDependency{}
+	resolveMessageDependency(msg.Desc, dep, map[string]bool{})
+
+	assert.Len(t, dep, 1)
 }
