fff2d1992eede84256fb0ac1fe97bd54cdeb7c79
ktr <aizu.s1230022@gmail.com>
2018-02-03 21:58:54 +0900

    fix repeated field inputting logic (#24)

--- entity/oneof_test.go
+++ entity/oneof_test.go
@@ -10,7 +10,7 @@ import (
 func TestOneOf(t *testing.T) {
 	d := parseFile(t, "oneof.proto")
 	msgs := d.GetMessageTypes()
-	require.Len(t, msgs, 1)
+	require.Len(t, msgs, 2)
 
 	m := newMessage(msgs[0])
 	require.Equal(t, m.Name(), "Example")
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	name    = "evans"
-	version = "0.2.0"
+	version = "0.2.1"
 )
 
 func main() {
