ea0ccadb64e0e2424bfada14a81381b17d6477ab
ktr0731 <aizu.s1230022@gmail.com>
2018-02-05 18:12:54 +0900

    move convertValue to protobuf package

--- adapter/gateway/prompt_inputter.go
+++ adapter/gateway/prompt_inputter.go
@@ -1,16 +1,14 @@
 package gateway
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/AlecAivazis/survey"
 	prompt "github.com/c-bata/go-prompt"
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/dynamic"
+	"github.com/ktr0731/evans/adapter/protobuf"
 	"github.com/ktr0731/evans/config"
 	"github.com/ktr0731/evans/entity"
 	"github.com/pkg/errors"
@@ -295,7 +293,7 @@ func (i *fieldInputter) inputPrimitiveField(req *dynamic.Message, field *desc.Fi
 		return i.inputPrimitiveField(req, field)
 	}
 
-	v, err := i.convertValue(in, field)
+	v, err := protobuf.ConvertValue(in, field)
 	if err != nil {
 		return err
 	}
@@ -308,71 +306,6 @@ func (i *fieldInputter) setField(req *dynamic.Message, field *desc.FieldDescript
 	}
 
 	return req.TrySetField(field, v)
-}
-
-// convertValue holds value and error of conversion
-// each cast (Parse*) returns falsy value when failed to parse argument
-func (i *fieldInputter) convertValue(pv string, f *desc.FieldDescriptor) (interface{}, error) {
-	var v interface{}
-	var err error
-
-	switch f.GetType() {
-	case descriptor.FieldDescriptorProto_TYPE_DOUBLE:
-		v, err = strconv.ParseFloat(pv, 64)
-
-	case descriptor.FieldDescriptorProto_TYPE_FLOAT:
-		v, err = strconv.ParseFloat(pv, 32)
-		v = float32(v.(float64))
-
-	case descriptor.FieldDescriptorProto_TYPE_INT64:
-		v, err = strconv.ParseInt(pv, 10, 64)
-
-	case descriptor.FieldDescriptorProto_TYPE_UINT64:
-		v, err = strconv.ParseUint(pv, 10, 64)
-
-	case descriptor.FieldDescriptorProto_TYPE_INT32:
-		v, err = strconv.ParseInt(pv, 10, 32)
-		v = int32(v.(int64))
-
-	case descriptor.FieldDescriptorProto_TYPE_UINT32:
-		v, err = strconv.ParseUint(pv, 10, 32)
-		v = uint32(v.(uint64))
-
-	case descriptor.FieldDescriptorProto_TYPE_FIXED64:
-		v, err = strconv.ParseUint(pv, 10, 64)
-
-	case descriptor.FieldDescriptorProto_TYPE_FIXED32:
-		v, err = strconv.ParseUint(pv, 10, 32)
-		v = uint32(v.(uint64))
-
-	case descriptor.FieldDescriptorProto_TYPE_BOOL:
-		v, err = strconv.ParseBool(pv)
-
-	case descriptor.FieldDescriptorProto_TYPE_STRING:
-		// already string
-		v = pv
-
-	case descriptor.FieldDescriptorProto_TYPE_BYTES:
-		v = []byte(pv)
-
-	case descriptor.FieldDescriptorProto_TYPE_SFIXED64:
-		v, err = strconv.ParseUint(pv, 10, 64)
-
-	case descriptor.FieldDescriptorProto_TYPE_SFIXED32:
-		v, err = strconv.ParseUint(pv, 10, 32)
-		v = int32(v.(int64))
-
-	case descriptor.FieldDescriptorProto_TYPE_SINT64:
-		v, err = strconv.ParseInt(pv, 10, 64)
-
-	case descriptor.FieldDescriptorProto_TYPE_SINT32:
-		v, err = strconv.ParseInt(pv, 10, 32)
-		v = int32(v.(int64))
-
-	default:
-		return nil, fmt.Errorf("invalid type: %#v", f.GetType())
-	}
-	return v, err
 }
 
 // makePrefix makes prefix for field f.
---
+++ adapter/protobuf/convert.go
@@ -0,0 +1,74 @@
+package protobuf
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"github.com/jhump/protoreflect/desc"
+)
+
+// ConvertValue holds value and error of conversion
+// each cast (Parse*) returns falsy value when failed to parse argument
+func ConvertValue(pv string, f *desc.FieldDescriptor) (interface{}, error) {
+	var v interface{}
+	var err error
+
+	switch f.GetType() {
+	case descriptor.FieldDescriptorProto_TYPE_DOUBLE:
+		v, err = strconv.ParseFloat(pv, 64)
+
+	case descriptor.FieldDescriptorProto_TYPE_FLOAT:
+		v, err = strconv.ParseFloat(pv, 32)
+		v = float32(v.(float64))
+
+	case descriptor.FieldDescriptorProto_TYPE_INT64:
+		v, err = strconv.ParseInt(pv, 10, 64)
+
+	case descriptor.FieldDescriptorProto_TYPE_UINT64:
+		v, err = strconv.ParseUint(pv, 10, 64)
+
+	case descriptor.FieldDescriptorProto_TYPE_INT32:
+		v, err = strconv.ParseInt(pv, 10, 32)
+		v = int32(v.(int64))
+
+	case descriptor.FieldDescriptorProto_TYPE_UINT32:
+		v, err = strconv.ParseUint(pv, 10, 32)
+		v = uint32(v.(uint64))
+
+	case descriptor.FieldDescriptorProto_TYPE_FIXED64:
+		v, err = strconv.ParseUint(pv, 10, 64)
+
+	case descriptor.FieldDescriptorProto_TYPE_FIXED32:
+		v, err = strconv.ParseUint(pv, 10, 32)
+		v = uint32(v.(uint64))
+
+	case descriptor.FieldDescriptorProto_TYPE_BOOL:
+		v, err = strconv.ParseBool(pv)
+
+	case descriptor.FieldDescriptorProto_TYPE_STRING:
+		// already string
+		v = pv
+
+	case descriptor.FieldDescriptorProto_TYPE_BYTES:
+		v = []byte(pv)
+
+	case descriptor.FieldDescriptorProto_TYPE_SFIXED64:
+		v, err = strconv.ParseUint(pv, 10, 64)
+
+	case descriptor.FieldDescriptorProto_TYPE_SFIXED32:
+		v, err = strconv.ParseUint(pv, 10, 32)
+		v = int32(v.(int64))
+
+	case descriptor.FieldDescriptorProto_TYPE_SINT64:
+		v, err = strconv.ParseInt(pv, 10, 64)
+
+	case descriptor.FieldDescriptorProto_TYPE_SINT32:
+		v, err = strconv.ParseInt(pv, 10, 32)
+		v = int32(v.(int64))
+
+	default:
+		return nil, fmt.Errorf("invalid type: %#v", f.GetType())
+	}
+	return v, err
+}
