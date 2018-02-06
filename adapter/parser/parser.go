804e53581d66112ee3316893c0fc8ae9c0ab9afe
ktr0731 <aizu.s1230022@gmail.com>
2018-02-06 12:25:44 +0900

    move Message to protobuf package

--- adapter/parser/parser.go
+++ adapter/parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"github.com/jhump/protoreflect/desc"
 	"github.com/ktr0731/evans/adapter/internal/proto_parser"
 	"github.com/ktr0731/evans/entity"
 )
@@ -10,5 +11,38 @@ func ParseFile(filename []string, paths []string) (entity.Packages, error) {
 	if err != nil {
 		return nil, err
 	}
-	return entity.ToEntitiesFrom(set)
+	return toEntitiesFrom(set)
+}
+
+// toEntitiesFrom normalizes descriptors to entities
+//
+// package
+// ├ messages
+// ├ enums
+// └ services
+//   └ rpcs
+//
+func toEntitiesFrom(files []*desc.FileDescriptor) (entity.Packages, error) {
+	var pkgNames []string
+	msgMap := map[string][]*entity.Message{}
+	svcMap := map[string][]*entity.Service{}
+	for _, f := range files {
+		pkgName := f.GetPackage()
+
+		pkgNames = append(pkgNames, pkgName)
+
+		for _, msg := range f.GetMessageTypes() {
+			msgMap[pkgName] = append(msgMap[pkgName], newMessage(msg))
+		}
+		for _, svc := range f.GetServices() {
+			svcMap[pkgName] = append(svcMap[pkgName], newService(svc))
+		}
+	}
+
+	var pkgs Packages
+	for _, pkgName := range pkgNames {
+		pkgs = append(pkgs, newPackage(pkgName, msgMap[pkgName], svcMap[pkgName]))
+	}
+
+	return pkgs, nil
 }
---
+++ adapter/protobuf/message.go
@@ -0,0 +1,73 @@
+package protobuf
+
+import (
+	"github.com/jhump/protoreflect/desc"
+	"github.com/ktr0731/evans/entity"
+)
+
+type Message struct {
+	fields []*field
+
+	desc      *desc.MessageDescriptor
+	fieldDesc *desc.FieldDescriptor // fieldDesc is nil if this message is not used as a field
+}
+
+func newMessage(m *desc.MessageDescriptor) *Message {
+	msg := entity.Message{
+		desc: m,
+	}
+
+	// TODO: label, map, options
+	fields := make([]*field, len(m.GetFields()))
+	for i, f := range m.GetFields() {
+		fields[i] = &field{
+			Name: f.GetName(),
+			Type: f.GetType().String(),
+		}
+	}
+	msg.fields = fields
+
+	return &msg
+}
+
+func newMessageAsField(f *desc.FieldDescriptor) *Message {
+	msg := newMessage(f.GetMessageType())
+	msg.fieldDesc = f
+	return msg
+}
+
+func (m *Message) isField() {}
+
+func (m *Message) Name() string {
+	if m.fieldDesc != nil {
+		return m.fieldDesc.GetName()
+	}
+	return m.desc.GetName()
+}
+
+func (m *Message) Type() string {
+	if m.fieldDesc == nil {
+		return ""
+	}
+	return m.fieldDesc.GetType().String()
+}
+
+func (m *Message) Number() int32 {
+	if m.fieldDesc == nil {
+		return NON_FIELD
+	}
+	return m.fieldDesc.GetNumber()
+}
+
+func (m *Message) IsRepeated() bool {
+	if m.fieldDesc == nil {
+		return false
+	}
+	return m.fieldDesc.IsRepeated()
+}
+
+func (m *Message) Fields() []*field {
+	return m.fields
+}
+
+type Messages []*Message
