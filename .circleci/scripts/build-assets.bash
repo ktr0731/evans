ef631b82c1cfb9917d5ad83ddaa20e9e21e88796
ktr0731 <aizu.s1230022@gmail.com>
2018-04-25 00:46:06 +0900

    add make deps

--- .circleci/scripts/build-assets.bash
+++ .circleci/scripts/build-assets.bash
@@ -5,6 +5,8 @@ OSARCH='darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm linux/arm64'
 mkdir pkg
 cd pkg
 
+make deps
+
 gox -osarch "$OSARCH" \
   -ldflags="-X github.com/ktr0731/evans/vendor/github.com/ktr0731/go-updater/github.isGitHubReleasedBinary=true" \
   ..
