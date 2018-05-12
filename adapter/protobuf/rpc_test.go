9157eb43182612476c7c1b9bd3d0d78fbaff98c6
ktr0731 <aizu.s1230022@gmail.com>
2018-05-12 14:30:34 +0900

    add IsClientStreaming test

--- adapter/protobuf/rpc_test.go
+++ adapter/protobuf/rpc_test.go
@@ -3,29 +3,30 @@ package protobuf
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRPC(t *testing.T) {
-	d := parseFile(t, "helloworld.proto")
+	d := parseFile(t, "stream.proto")
 	svcs := d.GetServices()
-	require.Len(t, svcs, 1)
+	assert.Len(t, svcs, 1)
 
 	msgs := d.GetMessageTypes()
-	require.Len(t, msgs, 2)
+	assert.Len(t, msgs, 2)
 
 	reqMsg := msgs[0]
 	resMsg := msgs[1]
 
 	svc := newService(svcs[0])
-	require.Len(t, svc.RPCs(), 1)
+	assert.Len(t, svc.RPCs(), 1)
 
 	rpc := svc.RPCs()[0]
 
-	require.Equal(t, "SayHello", rpc.Name())
-	require.Equal(t, "helloworld.Greeter.SayHello", rpc.FQRN())
-	require.Equal(t, reqMsg.GetName(), rpc.RequestMessage().Name())
-	require.Equal(t, len(reqMsg.GetFields()), len(rpc.RequestMessage().Fields()))
-	require.Equal(t, resMsg.GetName(), rpc.ResponseMessage().Name())
-	require.Equal(t, len(resMsg.GetFields()), len(rpc.ResponseMessage().Fields()))
+	assert.Equal(t, "SayHelloClientStreaming", rpc.Name())
+	assert.Equal(t, "helloworld.Greeter.SayHelloClientStreaming", rpc.FQRN())
+	assert.Equal(t, reqMsg.GetName(), rpc.RequestMessage().Name())
+	assert.Equal(t, len(reqMsg.GetFields()), len(rpc.RequestMessage().Fields()))
+	assert.Equal(t, resMsg.GetName(), rpc.ResponseMessage().Name())
+	assert.Equal(t, len(resMsg.GetFields()), len(rpc.ResponseMessage().Fields()))
+	assert.True(t, rpc.IsClientStreaming())
 }
