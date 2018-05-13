6fc4807290892f985bab45e322c7795388933106
ktr0731 <aizu.s1230022@gmail.com>
2018-05-13 11:32:00 +0900

    add more tests for StreamDesc

--- adapter/protobuf/rpc_test.go
+++ adapter/protobuf/rpc_test.go
@@ -1,12 +1,13 @@
 package protobuf
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRPC(t *testing.T) {
+func TestRPC_stream(t *testing.T) {
 	d := parseFile(t, "stream.proto")
 	svcs := d.GetServices()
 	assert.Len(t, svcs, 1)
@@ -16,17 +17,37 @@ func TestRPC(t *testing.T) {
 
 	reqMsg := msgs[0]
 	resMsg := msgs[1]
-
 	svc := newService(svcs[0])
-	assert.Len(t, svc.RPCs(), 1)
 
-	rpc := svc.RPCs()[0]
+	cases := []struct {
+		name                                 string
+		isClientStreaming, isServerStreaming bool
+	}{
+		{name: "SayHelloUnary"},
+		{name: "SayHelloClientStreaming", isClientStreaming: true},
+	}
 
-	assert.Equal(t, "SayHelloClientStreaming", rpc.Name())
-	assert.Equal(t, "helloworld.Greeter.SayHelloClientStreaming", rpc.FQRN())
-	assert.Equal(t, reqMsg.GetName(), rpc.RequestMessage().Name())
-	assert.Equal(t, len(reqMsg.GetFields()), len(rpc.RequestMessage().Fields()))
-	assert.Equal(t, resMsg.GetName(), rpc.ResponseMessage().Name())
-	assert.Equal(t, len(resMsg.GetFields()), len(rpc.ResponseMessage().Fields()))
-	assert.True(t, rpc.IsClientStreaming())
+	assert.Len(t, svc.RPCs(), len(cases))
+
+	for i, c := range cases {
+		rpc := svc.RPCs()[i]
+		assert.Equal(t, c.name, rpc.Name())
+		assert.Equal(t, fmt.Sprintf("helloworld.Greeter.%s", c.name), rpc.FQRN())
+		assert.Equal(t, reqMsg.GetName(), rpc.RequestMessage().Name())
+		assert.Equal(t, len(reqMsg.GetFields()), len(rpc.RequestMessage().Fields()))
+		assert.Equal(t, resMsg.GetName(), rpc.ResponseMessage().Name())
+		assert.Equal(t, len(resMsg.GetFields()), len(rpc.ResponseMessage().Fields()))
+		assert.Equal(t, c.isClientStreaming, rpc.IsClientStreaming())
+		assert.Equal(t, c.isServerStreaming, rpc.IsServerStreaming())
+
+		if !c.isClientStreaming && !c.isServerStreaming {
+			assert.Panics(t, func() {
+				rpc.StreamDesc()
+			})
+		} else {
+			assert.NotPanics(t, func() {
+				rpc.StreamDesc()
+			})
+		}
+	}
 }
