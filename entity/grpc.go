78b59814fda30ebef0cc72f8c30ca2392dbc47dd
ktr0731 <aizu.s1230022@gmail.com>
2018-05-13 12:05:05 +0900

    add ClientStream interface and implementation

--- entity/grpc.go
+++ entity/grpc.go
@@ -2,19 +2,39 @@ package entity
 
 import (
 	"context"
+	"io"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
 
 type GRPCClient interface {
 	Invoke(ctx context.Context, fqrn string, req, res interface{}) error
-	NewClientStream(ctx context.Context, rpc RPC) grpc.ClientStream
+	NewClientStream(ctx context.Context, rpc RPC) ClientStream
+}
+
+type ClientStream interface {
+	Send(proto.Message) error
+	CloseAndReceive() (proto.Message, error)
 }
 
 type clientStream struct {
-	grpc.ClientStream
+	cs grpc.ClientStream
 }
 
 func newClientStream(cs grpc.ClientStream) *clientStream {
 	return &clientStream{cs}
+}
+
+func (s *clientStream) Send(m proto.Message) error {
+	return s.cs.SendMsg(m)
+}
+
+func (s *clientStream) CloseAndReceive(res proto.Message) error {
+	err := s.cs.RecvMsg(res)
+	if err != nil && err != io.EOF {
+		return errors.Wrap(err, "failed to close and receive response")
+	}
+	return nil
 }
