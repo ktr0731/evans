2b7b03e6c6324b27cbd5e4970d4b047d735a0a2c
ktr0731 <aizu.s1230022@gmail.com>
2018-05-19 09:23:59 +0900

    add parser_test.go

---
+++ adapter/parser/parser_test.go
@@ -0,0 +1,23 @@
+package parser
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseFile(t *testing.T) {
+	t.Run("importing", func(t *testing.T) {
+		fnames := []string{testdata("importing", "library")}
+		pkgs, err := ParseFile(fnames, nil)
+		require.NoError(t, err)
+		assert.Len(t, pkgs, 1)
+		assert.Len(t, pkgs[0].Messages, 4)
+	})
+}
+
+func testdata(s ...string) string {
+	return filepath.Join(s...)
+}
