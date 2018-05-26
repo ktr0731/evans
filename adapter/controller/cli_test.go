dcad77cb0e4caeca60ded1686732f56e57e5d362
ktr0731 <aizu.s1230022@gmail.com>
2018-05-26 10:13:47 +0900

    add mergeConfig tests

--- adapter/controller/cli_test.go
+++ adapter/controller/cli_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/ktr0731/evans/config"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -68,4 +69,47 @@ func TestCLI(t *testing.T) {
 		_, err := resolveProtoPaths(cfg)
 		require.Error(t, err)
 	})
+}
+
+func Test_mergeConfig(t *testing.T) {
+	// setup
+	// append elements to slice which will be merged.
+	cfg := &config.Config{
+		Default: &config.Default{
+			Package:   "tamaki",
+			ProtoPath: []string{"kobuchizawa"},
+			ProtoFile: []string{"miyake"},
+		},
+		Server: &config.Server{
+			Port: "50052",
+		},
+		Request: &config.Request{
+			Header: []config.Header{{Key: "yuzuki", Val: "shiraishi"}},
+		},
+		Env:  &config.Env{},
+		REPL: &config.REPL{},
+	}
+	config.SetupConfig(cfg)
+
+	opt := &options{
+		pkg:     "kumiko",
+		service: "reina",
+		path:    []string{"midori"},
+		host:    "hazuki",
+		port:    "50053",
+		header:  []string{"nozomi=kasaki"},
+	}
+	proto := []string{"noboru"}
+
+	res, err := mergeConfig(cfg, opt, proto)
+	require.NoError(t, err)
+	assert.Equal(t, res.REPL.Server, res.Server)
+	assert.Equal(t, res.Env.Server, res.Server)
+	assert.Equal(t, opt.pkg, res.Default.Package)
+	assert.Equal(t, opt.service, res.Default.Service)
+	assert.Equal(t, append(cfg.Default.ProtoPath, opt.path...), res.Default.ProtoPath)
+	assert.Equal(t, append(cfg.Default.ProtoFile, proto...), res.Default.ProtoFile)
+	assert.Equal(t, opt.host, res.Server.Host)
+	assert.Equal(t, opt.port, res.Server.Port)
+	assert.Equal(t, append(cfg.Request.Header, config.Header{Key: "nozomi", Val: "kasaki"}), res.Request.Header)
 }
