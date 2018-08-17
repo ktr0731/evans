638748badd37905289dfa41c79b5edf7d6e74e01
Carl Baldwin <carl@ecbaldwin.net>
2018-08-17 03:37:32 -0600

    Stop putting directories from the file names onto the import path (#82)
    This behavior was incorrect and led to strange behaviors. Proto files are meant
    to be importable using hierarchical names which should be found relative to the
    import paths. Hence, adding the directory part of the given path names to the
    import path only encourages incorrect use of hierarchical names
    
    For example, if my proto file is named api.proto and is not meant to be
    imported with a hierarchical name, it is incorrect to allow me to specify it as
    `path/to/api.proto` because if I import a second file which happens to import
    `api.proto` the parser has no way of knowing that it is supposed to be the same
    as `path/to/api.proto` which I originally gave it.
    
    Addresses issue #80

--- adapter/internal/protoparser/parser.go
+++ adapter/internal/protoparser/parser.go
@@ -1,27 +1,13 @@
 package protoparser
 
 import (
-	"path/filepath"
-
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/desc/protoparse"
 )
 
 func ParseFile(fnames []string, paths []string) ([]*desc.FileDescriptor, error) {
-	encountered := map[string]bool{}
 	paths = append(paths, ".")
-	encountered["."] = true
-	for _, path := range paths {
-		encountered[path] = true
-	}
 
-	for _, fname := range fnames {
-		path := filepath.Dir(fname)
-		if !encountered[path] {
-			paths = append(paths, path)
-			encountered[path] = true
-		}
-	}
 	p := &protoparse.Parser{
 		ImportPaths: paths,
 	}
--- adapter/parser/parser_test.go
+++ adapter/parser/parser_test.go
@@ -11,7 +11,7 @@ import (
 func TestParseFile(t *testing.T) {
 	t.Run("importing", func(t *testing.T) {
 		fnames := []string{testdata("importing", "library.proto")}
-		pkgs, err := ParseFile(fnames, nil)
+		pkgs, err := ParseFile(fnames, []string{"testdata/importing"})
 		require.NoError(t, err)
 		assert.Len(t, pkgs, 1)
 		assert.Len(t, pkgs[0].Messages, 4)
