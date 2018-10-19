f8bb9b112d41637efed2011c7d1d7129f12c7070
ktr0731 <ktr@syfm.me>
2018-10-19 11:28:36 +0900

    add WithIndent test

--- adapter/presenter/cli_test.go
+++ adapter/presenter/cli_test.go
@@ -29,3 +29,27 @@ func TestCLIPresenter(t *testing.T) {
 		require.Equal(t, `{"name":"makise","message":"kurisu"}`+"\n", string(b))
 	})
 }
+
+func TestJSONCLIPresenterWithIndent(t *testing.T) {
+	presenter := NewJSONCLIPresenterWithIndent()
+
+	t.Run("Call", func(t *testing.T) {
+		descs := testhelper.ReadProtoAsFileDescriptors(t, "helloworld.proto")
+		msg := testhelper.FindMessage(t, "HelloRequest", descs)
+
+		dmsg := dynamic.NewMessage(msg)
+		dmsg.SetField(dmsg.FindFieldDescriptorByName("name"), "makise")
+		dmsg.SetField(dmsg.FindFieldDescriptorByName("message"), "kurisu")
+
+		out, err := presenter.Call(dmsg)
+		require.NoError(t, err)
+
+		b, err := ioutil.ReadAll(out)
+		require.NoError(t, err)
+
+		require.Equal(t, `{
+  "name": "makise",
+  "message": "kurisu"
+}`+"\n", string(b))
+	})
+}
