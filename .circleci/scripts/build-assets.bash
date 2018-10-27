786775df9f4d74c03f9fedc0d04b00e4eab42484
ktr0731 <ktr@syfm.me>
2018-10-27 10:10:54 +0900

    fix build-assets.bash again

--- .circleci/scripts/build-assets.bash
+++ .circleci/scripts/build-assets.bash
@@ -10,11 +10,14 @@ gox -osarch "$OSARCH" \
   ..
 
 for f in *; do
+  # Windows
   if echo "$f" | grep exe > /dev/null 2>&1; then
     mv "$f" evans.exe
+    tar czvf "$f.tar.gz" evans.exe
+    rm -f evans.exe
   else
     mv "$f" evans
+    tar czvf "$f.tar.gz" evans
+    rm -f evans
   fi
-  tar czvf "$f.tar.gz" evans
-  rm -f evans
 done
