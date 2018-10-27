f213c609a6f63b8bd90d52afc26b4ad6ebc41f46
ktr0731 <ktr@syfm.me>
2018-10-27 09:20:21 +0900

    support Windows artifacts

--- .circleci/scripts/build-assets.bash
+++ .circleci/scripts/build-assets.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-OSARCH='darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm linux/arm64'
+OSARCH='darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm linux/arm64 windows/386 windows/amd64'
 
 mkdir pkg
 cd pkg
