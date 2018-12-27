012bd205bf8202f6a8bfdc1af7d298efd0c75d92
ktr <ktr@syfm.me>
2018-12-27 21:05:47 +0900

    tagging "v" prefixed version when creating a new release (#144)

--- .circleci/scripts/create-new-release.bash
+++ .circleci/scripts/create-new-release.bash
@@ -8,6 +8,8 @@ fi
 VERSION=$1
 
 git tag "$VERSION"
+git tag "v$VERSION" "$VERSION" # tagging another one which has "v" prefix for Go modules.
 git push origin "$VERSION"
+git push origin "v$VERSION"
 
 ghr -parallel=1 "$VERSION" pkg
