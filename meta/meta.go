7b7404f294775a65517949ec6a6fa5c3b6cb8e1f
ktr0731 <ktr@syfm.me>
2019-09-11 23:00:08 +0900

    bump to v0.8.3

--- meta/meta.go
+++ meta/meta.go
@@ -5,5 +5,5 @@ import version "github.com/hashicorp/go-version"
 const AppName = "evans"
 
 var (
-	Version = version.Must(version.NewSemver("0.8.2"))
+	Version = version.Must(version.NewSemver("0.8.3"))
 )
