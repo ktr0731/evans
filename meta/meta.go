fdcca4e7638d0ca35d3f8b51a7bbd895670689ff
ktr0731 <ktr@syfm.me>
2022-01-22 15:43:41 +0900

    bump to v0.10.2

--- meta/meta.go
+++ meta/meta.go
@@ -5,5 +5,5 @@ import version "github.com/hashicorp/go-version"
 const AppName = "evans"
 
 var (
-	Version = version.Must(version.NewSemver("0.10.1"))
+	Version = version.Must(version.NewSemver("0.10.2"))
 )
